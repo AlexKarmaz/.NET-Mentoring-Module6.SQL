36fd014c575c41d29ae0a14f3936f600f16cb5cf
AlexKarmaz <karmazct@mail.ru>
2018-04-28 02:29:15 +0300

    add a part of task2

---
+++ Northwind(tasks 1-2)/Task2/Task2.1/Subtask2.1.3.sql
@@ -0,0 +1,5 @@
+﻿--По таблице Orders найти количество различных покупателей (CustomerID), сделавших заказы. Использовать функцию COUNT
+--и не использовать предложения WHERE и GROUP.
+
+SELECT COUNT(DISTINCT Orders.[CustomerId]) AS 'Distinct customers count' 
+FROM [dbo].[Orders] Orders;
