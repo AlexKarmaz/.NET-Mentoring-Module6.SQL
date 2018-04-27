0eed75a10381ee29750dade6842690edd27d0f69
AlexKarmaz <karmazct@mail.ru>
2018-04-28 02:05:13 +0300

    add task1

---
+++ Northwind(tasks 1-2)/Task1/Task1.3/Subtask1.3.2.sql
@@ -0,0 +1,10 @@
+﻿--Выбрать всех заказчиков из таблицы Customers, у которых название страны начинается на буквы из диапазона b и g. 
+--Использовать оператор BETWEEN. Проверить, что в результаты запроса попадает Germany. Запрос должен возвращать 
+--только колонки CustomerID и Country и отсортирован по Country.
+
+SELECT 
+    Customers.[CustomerId]     AS 'CustomerId',
+	Customers.[Country]       AS 'Country'
+FROM [dbo].[Customers] Customers
+WHERE SUBSTRING(Customers.[Country], 1, 1) BETWEEN 'b' AND 'g'
+ORDER BY Customers.[Country];
